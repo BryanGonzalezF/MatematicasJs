8f636c4a8ded6128a9b180c17c4370ed37835e0f
Bryan Gonzalez <brygon.2015@gmail.com>
2023-09-14 12:12:17 -0600

    se agrego el inicio del ejercicio de moda y se hizo una prueba de ejercicio de sumar arrays

---
+++ Estadísticas/moda.js
@@ -0,0 +1,18 @@
+function calcularModa(lista) {
+  const listaCount = {};
+  for (let i = 0; i < lista.length; i++) {
+    const elementoLista = lista[i];
+    listaCount[elementoLista] = 1;
+  }
+}
+
+/* function sumaArr(lista) {
+  let suma = 0;
+  for (let i = 0; i < lista.length; i++) {
+    suma = suma + lista[i];
+  }
+  console.log(suma);
+}
+
+sumaArr([1, 2, 3]);
+ */
