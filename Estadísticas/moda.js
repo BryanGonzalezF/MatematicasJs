25bec5ddd5e58a262c3bdc9939ef4b433745d0eb
Bryan Gonzalez <brygon.2015@gmail.com>
2023-09-18 16:44:14 -0600

    finalizacion de la clase de moda

--- Estadísticas/moda.js
+++ Estadísticas/moda.js
@@ -2,10 +2,27 @@ function calcularModa(lista) {
   const listaCount = {};
   for (let i = 0; i < lista.length; i++) {
     const elementoLista = lista[i];
-    listaCount[elementoLista] = 1;
-  }
-}
 
+    if (listaCount[elementoLista]) {
+      listaCount[elementoLista] += 1;
+    } else {
+      listaCount[elementoLista] = 1;
+    }
+  }
+
+  const listaArr = Object.entries(listaCount);
+  const listaOrdenada = ordenarListaBidimensional(listaArr, 1);
+  const MaxNumberLista = listaOrdenada[listaArr.length - 1];
+  /* console.log({ listaCount, listaArr, listaOrdenada, MaxNumberLista }); */
+  console.log('La moda de su lista es', MaxNumberLista[0]);
+}
+function ordenarListaBidimensional(listaDesordenada, i) {
+  function ordenarListasSort(valorAcumulado, nuevoValor) {
+    return valorAcumulado[i] - nuevoValor[i];
+  }
+  const lista = listaDesordenada.sort(ordenarListasSort);
+  return lista;
+}
 /* function sumaArr(lista) {
   let suma = 0;
   for (let i = 0; i < lista.length; i++) {
